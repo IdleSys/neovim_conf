a8f3b1bd8cf23caaad550dd3ccadbc57e8221d39
= <sudo.bahrami.alireza@gmail.com>
2024-12-28 05:58:19 +0330

    chore: restructering some other plugins

--- lua/plugins/theme/onedark.lua
+++ lua/plugins/theme/onedark.lua
@@ -1,3 +1,10 @@
 return {
   'navarasu/onedark.nvim',
+  config = function(_, opts)
+    require('onedark').setup {
+      style = 'deep',
+    }
+    require('onedark').load()
+    vim.cmd 'hi ColorColumn ctermbg=235 guibg=#3E3B2F'
+  end,
 }
