9c7d544ba2a69cc48b04a569a73951fff3212e29
= <sudo.bahrami.alireza@gmail.com>
2025-01-13 00:31:36 +0330

    feat: adding snacks nvim

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,83 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = false },
+    buffdelete = { enabled = false },
+    dashboard = {
+      sections = {
+        { section = 'header' },
+        {
+          pane = 2,
+          section = 'terminal',
+          cmd = 'colorscript -e square',
+          height = 5,
+          padding = 1,
+        },
+        { section = 'keys', gap = 1, padding = 1 },
+        { pane = 2, icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+        { pane = 2, icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+        {
+          pane = 2,
+          icon = ' ',
+          title = 'Git Status',
+          section = 'terminal',
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = 'git status --short --branch --renames',
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        },
+        {
+          icon = ' ',
+          title = 'Sessions',
+          section = 'projects',
+          indent = 2,
+          padding = 1,
+          dirs = function()
+            -- ...
+            return {}
+          end,
+          action = function(dir)
+            print 'does not get called after resize'
+          end,
+        },
+      },
+    },
+    debug = { enabled = false },
+    indent = { enabled = true },
+    input = { enabled = false },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
+  keys = {
+    {
+      '<leader>`',
+      function()
+        Snacks.scratch()
+      end,
+      desc = 'Toggle Scratch Buffer',
+    },
+    {
+      '<leader>s`',
+      function()
+        Snacks.scratch.select()
+      end,
+      desc = 'Select Scratch Buffer',
+    },
+    {
+      '<leader>T',
+      function()
+        Snacks.terminal.open()
+      end,
+    },
+  },
+}
