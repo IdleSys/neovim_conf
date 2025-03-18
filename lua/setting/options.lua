5ee579e462c5f17750ea085615a56919f1cbcf2a
= <sudo.bahrami.alireza@gmail.com>
2025-03-18 15:30:56 +0330

    feat(setting): overriding hi groups to achieve transparency

--- lua/setting/options.lua
+++ lua/setting/options.lua
@@ -10,8 +10,7 @@ vim.opt.relativenumber = true
 vim.opt.mouse = 'a'
 vim.opt.showmode = false
 vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 vim.opt.colorcolumn = '+1'
 vim.opt.textwidth = 80
 
@@ -35,3 +34,27 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
+
+-- Setting highlight colors to transparent
+local highlights = {
+  'Normal', -- General background for text
+  'CursorLine', -- Background for current line highlight
+  'StatusLine', -- Background for status line
+  'VertSplit', -- Background for vertical splits
+  'SignColumn', -- Background for the sign column (left of line numbers)
+  'Pmenu', -- Popup menu background
+  'PmenuSel', -- Popup menu selected item background
+  'TabLine', -- Background for tab line
+  'TabLineSel', -- Background for selected tab line
+  'EndOfBuffer', -- Background for empty space at the end of the buffer
+  'NonText', -- Background for non-text (like invisible characters)
+  'FoldColumn', -- Background for fold column
+  'DiffAdd', -- Background for added lines in diff
+  'DiffChange', -- Background for changed lines in diff
+  'DiffDelete', -- Background for deleted lines in diff
+  'DiffText', -- Background for changed sections in diff
+}
+
+for _, group in ipairs(highlights) do
+  vim.api.nvim_set_hl(0, group, { ctermbg = 'NONE', bg = 'NONE' })
+end
