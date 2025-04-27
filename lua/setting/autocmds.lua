2b1d6c5ce1b6fdb434fecf0910e3958aa855ef09
= <sudo.bahrami.alireza@gmail.com>
2025-04-27 15:05:00 +0330

    feat(autocmds): adding auto cmd to set window title to projects root directory

--- lua/setting/autocmds.lua
+++ lua/setting/autocmds.lua
@@ -12,3 +12,31 @@ vim.api.nvim_create_autocmd('User', {
     require('snacks').rename.on_rename_file(event.data.from, event.data.to)
   end,
 })
+
+-- [[ Setting nvim title to project root directory]]
+vim.api.nvim_create_augroup('SetProjectTitle', { clear = true })
+
+vim.api.nvim_create_autocmd('VimEnter', {
+  group = 'SetProjectTitle',
+  pattern = '*',
+  callback = function()
+    -- Don't set the title if you just opened Neovim
+    if vim.fn.argc() == 0 then
+      return
+    end
+    -- Set the title based on the project directory name
+    local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+    vim.opt.titlestring = project_name
+    vim.opt.title = true
+  end,
+})
+
+vim.api.nvim_create_autocmd('DirChanged', {
+  group = 'SetProjectTitle',
+  pattern = '*',
+  callback = function()
+    local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+    vim.opt.titlestring = project_name
+    vim.opt.title = true
+  end,
+})
